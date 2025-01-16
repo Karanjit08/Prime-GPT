f302bde2e73b73f5af30e985cbd99d6fab40ea39
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-01-16 20:24:51 +0530

    Welcome Screen footer

--- src/component/Welcome_Footer.js
+++ src/component/Welcome_Footer.js
@@ -3,7 +3,10 @@
 var WelcomeFooter = () => {
     return (
         <div className="welcome-footer">
-
+        <img src="https://m.media-amazon.com/images/G/01/digital/video/acquisition/web_footer_logo._CB462908456_.png" alt="footer-logo"></img>
+        <div className="terms-condition">
+        
+        </div>
         </div>
     );
 }
