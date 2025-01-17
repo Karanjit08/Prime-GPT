8d3767de9a0e851256264e809d36a739f4ac9f78
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-01-17 22:19:36 +0530

    Welcome Screen footer

--- src/component/Welcome_Footer.js
+++ src/component/Welcome_Footer.js
@@ -5,7 +5,12 @@ var WelcomeFooter = () => {
         <div className="welcome-footer">
         <img src="https://m.media-amazon.com/images/G/01/digital/video/acquisition/web_footer_logo._CB462908456_.png" alt="footer-logo"></img>
         <div className="terms-condition">
-        
+        <p>Terms & Privacy Notice</p>
+        <p>Send us feedback</p>
+        <p>Help</p>
+        </div>
+        <div className="affiliate">
+        © 1996-2025, Amazon.com, Inc. or its affiliates
         </div>
         </div>
     );
