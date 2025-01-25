9228c072d469f5aec4d754053b8ee0a69f9dc5e4
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-01-25 23:04:28 +0530

    Constants configuration

--- src/component/Welcome_Header.js
+++ src/component/Welcome_Header.js
@@ -1,6 +1,7 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
+import { header_home_text, header_liveTv_text, header_movies_text, header_subscriptions_text, header_tvShow_text, header_joinPrime_text } from "../utils/constants";
 
 var WelcomeHeader = () => {
   return (
@@ -12,17 +13,17 @@ var WelcomeHeader = () => {
             alt="prime-logo"
           ></img>
         </div>
-        <a>Home</a>
-        <a>Movies</a>
-        <a>TV Shows</a>
-        <a>Live TV</a>
-        <a>Subcriptions</a>
+        <a>{header_home_text}</a>
+        <a>{header_movies_text}</a>
+        <a>{header_tvShow_text}</a>
+        <a>{header_liveTv_text}</a>
+        <a>{header_subscriptions_text}</a>
       </div>
       <div className="welcome-header-right">
         <div className="header-search-icon">
           <FontAwesomeIcon icon={faMagnifyingGlass} />
         </div>
-        <button className="join-prime-header"><Link to={"/login"} className="join-prime-link">Join Prime</Link></button>
+        <button className="join-prime-header"><Link to={"/login"} className="join-prime-link">{header_joinPrime_text}</Link></button>
 
       </div>
     </div>
---
+++ src/utils/constants.js
@@ -0,0 +1,8 @@
+export const header_home_text = "Home";
+export const header_movies_text = "Movies";
+export const header_tvShow_text = "TV Shows";
+export const header_liveTv_text = "Live TV";
+export const header_subscriptions_text = "Subcriptions";
+export const header_joinPrime_text = "Join Prime";
+
+
