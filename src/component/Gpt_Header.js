ce4e9386563cf94be600132f387cabb67653bacd
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-02-14 23:39:28 +0530

    Gemini API Implementation

--- src/component/Gpt_Header.js
+++ src/component/Gpt_Header.js
@@ -1,7 +1,8 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 import { useRef } from "react";
-import client from "../utils/open_ai";
+
+
 
 const GptHeader = () => {
   var searchText = useRef();
@@ -11,13 +12,6 @@ const GptHeader = () => {
       "Act as a Movie Recommendation system and suggest some movies for the query : " +
       searchText.current.value +
       ". only give me names of 5 movies, comma seperated like the example result given ahead. Example Result: Gadar, Sholay, Don, Golmaal, Koi Mil Gaya";
-
-    const chatCompletion = await client.chat.completions.create({
-      messages: [{ role: "user", content: gptQuery }],
-      model: "gpt-4o",
-    });
-
-    console.log(chatCompletion);
   };
 
   return (
