a9fa0d7ccae5e1167d3b2a56efab8b74bc1333db
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-02-14 15:56:01 +0530

    GPT Implementation

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,3 @@
 export const baseImageUrl = "https://image.tmdb.org/t/p/w200";
-export const OPENAI_API_KEY = "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";
 
 
