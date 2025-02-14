2d445a1e3a67530820d0332b0a8b1c926993339d
Karanjit08 <karanjitrandhawa2000@gmail.com>
2025-02-14 15:56:56 +0530

    GPT Implementation

--- src/utils/open_ai.js
+++ src/utils/open_ai.js
@@ -1,9 +1,9 @@
 import OpenAI from "openai/index.mjs";
-import { OPENAI_API_KEY } from "./constants";
+
 
 
 const client = new OpenAI({
-    apiKey: OPENAI_API_KEY, // This is the default and can be omitted
+    // apiKey: OPENAI_API_KEY, // This is the default and can be omitted
     dangerouslyAllowBrowser: true
   });
 
